1fe2ac848626eba9f7405cd51fb217b977bb19da
yanghucheng <chengpro@vip.qq.com>
2020-11-14 18:47:24 +0800

    upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # golang学习笔记
+
+## [golang学习笔记](https://blog.samlog.club/archives/702/)
+
+## [基础语法](./基础语法.md)
--- basic/branch.go
+++ basic/branch.go
@@ -5,11 +5,38 @@ import (
 	"io/ioutil"
 )
 
-func main() {
+
+func grade(score int) string {
+	s := ""
+	switch {
+	case 0 > score || score > 100:
+		panic(fmt.Sprintf("Error score.. %d", score))
+	case score < 60:
+		s = "D"
+	case score < 80:
+		s = "C"
+	case score < 90:
+		s = "B"
+	case score <= 100:
+		s = "A"
+	default:
+		panic(fmt.Sprintf("Error score %d", score))
+	}
+	return s
+}
+
+func getContents() {
 	const filename = "abc.txt"
 	if c, err := ioutil.ReadFile(filename); err != nil {
 		fmt.Println(err)
 	} else {
 		fmt.Printf("contents: %s\n", c)
 	}
+}
+
+func main() {
+
+	getContents()
+	s := grade(10)
+	fmt.Println(s)
 }
